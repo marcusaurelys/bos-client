0607d8b5040fdc49cc8b8267cd9219f15e16447f
christianvtia <christiantia04@gmail.com>
2024-06-16 00:58:39 +0800

    Implement NavBar links and profile dropdown
    Information is still hardcoded

--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -1,25 +1,73 @@
+'use client'
+
 import React from 'react'
 import Link from "next/link"
 import { Sheet, SheetTrigger, SheetContent } from "@/components/ui/sheet"
 import { Button } from "@/components/ui/button"
+import { usePathname } from 'next/navigation'
+import { Separator } from '@/components/ui/separator'
+import { UserSession } from '@/types'
+import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover'
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
+import { Command, CommandDialog, CommandItem, CommandList } from '@/components/ui/command'
+import { DropdownMenu, DropdownMenuContent, DropdownMenuGroup, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from '@/components/ui/dropdown-menu'
 
+interface NavBarProps {
+  user: UserSession | null
+}
 
-function NavBar() {
+function NavBar(user: NavBarProps) {
+  const pathName = usePathname()
+
   return (
-    <header className="flex items-center justify-between px-4 py-4 bg-gray-900 text-white shadow-md md:px-6">
+    <header className="flex items-center justify-between px-4 py-3 text-primary border-b  md:px-6">
       <Link href="#" className="flex items-center gap-2" prefetch={false}>
-        <span className="text-lg font-semibold">businessOS</span>
+        <span className="text-lg font-semibold text-primary">businessOS</span>
       </Link>
       <nav className="hidden md:flex items-center gap-4">
-        <Link href="/" className="text-sm font-medium hover:underline" prefetch={false}>
-          KanbanBoard
+        <Link href="/" className={`text-sm font-medium hover:underline ${pathName == "/" ? "text-sky-600": ""}`} prefetch={false}>
+          Board
         </Link>
-        <Link href="/admin" className="text-sm font-medium hover:underline" prefetch={false}>
+        <Link href="/admin" className={`text-sm font-medium hover:underline ${pathName.startsWith("/admin") ? "text-sky-600": ""}`} prefetch={false}>
           Team
         </Link>
-        <Link href="/login" className="text-sm font-medium hover:underline" prefetch={false}>
-          Login
-        </Link>
+        <Separator className="h-4" orientation="vertical" />
+        {
+          user 
+          ? 
+          <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <Avatar className="h-6 w-6 cursor-pointer">
+                  <AvatarImage src={""}/>
+                  <AvatarFallback className="bg-red-500 text-white text-xs">JB</AvatarFallback>
+                </Avatar>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent align="end">
+                <DropdownMenuLabel>
+                  <div className="flex flex-col">
+                    <p className="text-md">Joel Batac</p>
+                    <p className="text-md text-primary/50">joel_batac@dlsu.edu.ph</p>
+                  </div>
+                </DropdownMenuLabel>
+                <DropdownMenuSeparator />
+                <DropdownMenuGroup>
+                  <Link href="/profile"><DropdownMenuItem>Profile</DropdownMenuItem></Link>
+                  <Link href="/settings"><DropdownMenuItem>Settings</DropdownMenuItem></Link>
+                </DropdownMenuGroup>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem className="text-red-600">Log out</DropdownMenuItem>
+
+
+
+              </DropdownMenuContent>
+          </DropdownMenu>
+          : 
+          <Link href="/login" className={`text-sm font-medium hover:underline ${pathName.startsWith("/login") ? "text-sky-600": ""}`} prefetch={false}>
+            Login
+          </Link> 
+
+        }
+        
       </nav>
       
     </header>
