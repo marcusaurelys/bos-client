f9ce34e269f7beda6247072743dd0b3aeed47ca8
BorisVictoria <boris_victoria@dlsu.edu.ph>
2024-06-20 18:22:15 +0800

    refactor: remove unnecessary context call in Board

--- src/app/components/Board.tsx
+++ src/app/components/Board.tsx
@@ -1,32 +1,8 @@
 'use client'
-
-import React, { useState, useEffect } from 'react'
-import { useDataContext } from '@/contexts/DataContext'
 import Column from './Column'
 import Filter from './Filter';
-import { WithId } from 'mongodb';
-import { ITicket } from '@/types';
 
 export default function Board() {
-    const { tickets, setTickets } = useDataContext()
-    
-    // const [filteredTickets, setFilteredTickets] = useState(tickets)
-
-    // useEffect(() => {
-    //   let filtered = [...tickets]
-    
-    //   if(filters.size > 0) {
-    //     let temps = Array.from(filters).map((filter => {
-    //       let temp = tickets.filter((t) => t.priority === filter)
-    //       return temp
-    //     }))
-    //     console.log(temps.flat())
-    //     filtered = temps.flat()
-    //   }
-
-    //   setFilteredTickets(t => filtered)
-    // }, [filters])
-
   return (
     <div className="flex justify-center items-start w-full h-full">
       <div className="flex flex-col w-fit justify-center">
@@ -40,6 +16,5 @@ export default function Board() {
         </div>
       </div>   
     </div>
-    
   )
 }
