435a0bf79ec6f6af018cdbe26b1b0baec4db3a06
Joel Ethan Batac <bjoelethan@gmail.com>
2024-06-24 01:42:40 +0800

    Fix css not serving on login redirect

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "@/app/globals.css";
 import NavBar from "../components/NavBar";
 import DataContextProvider from '@/contexts/DataContext'
 import { cookies } from 'next/headers'
