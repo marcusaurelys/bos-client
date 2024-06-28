0285d330502026fcf9600ddf0a1a7d7db7cda24e
BorisVictoria <boris_victoria@dlsu.edu.ph>
2024-06-28 22:15:23 +0800

    feat: add chatbot history server actions

--- src/db/chat.ts
+++ src/db/chat.ts
@@ -1,13 +1,19 @@
 'use server'
 
 import { Base64 } from 'js-base64'
-   
+import { ObjectID } from 'mongo'   
+import { cookies } from 'next/headers'
+import { useDB } from '@/db/mongo'
+import { get_user_by_token } from '@/db/users'
 
 const CRISP_WEBSITE_ID = process.env.CRISP_WEBSITE_ID
 const CRISP_API_ID = process.env.CRISP_API_ID
 const CRISP_API_KEY = process.env.CRISP_API_KEY
 const CHATBOT_URL = process.env.CHATBOT_URL
 const CHATBOT_API_KEY = process.env.CHATBOT_API_KEY
+
+const db = await useDB()
+const chats = db.collection('chats')
 
 export const fuckNextChat = async() => {
   
@@ -60,7 +66,63 @@ export const get_chatbot_response = async(prompt) => {
 
 // Chatbot History 
 
-export const get_chatbot_history = async() => {
-  
+/*
+Chat Object
+  {
+    id_: ObjectID // irrelevant in this case
+    user_id: ObjectID // user id in user collection
+    chat_id: string // 'session_id' of conversation in Crisp
+    chats: [{
+      sender: string
+      message: string
+    }]
+  }
+*/
+
+export const get_chat_history_of_user = async() => {
+  const token = cookies().get('session')
+  const user = await get_user_by_token(token.value)
+
+  // No need to verify for user validity since the middleware should prevent this request from running if the token is invalid
+  const user_id = user._id
+
+  const response = await chats.find({user_id: user_id})
+  const result = await response.json()
+  return result 
 }
 
+export const get_chat_history_of_ticket = async(session_id: string) => {
+  const token = cookies().get('session')
+  const user = await get_user_by_token(token.value)
+
+  // No need to verify for user validity since the middleware should prevent this request from running if the token is invalid
+  const user_id = user._id
+  
+  const response = await chats.findOne({user_id: user_id, session_id: session_id})
+  const result = await response.json()
+  return result
+
+}
+
+// Universal update, delete, patch, append. Pass an array here.
+export const update_chat_history_of_ticket = async(session_id: string, chats: Chat[]) => {
+  const token = cookies().get('session')
+  const user = await get_user_by_token(token.value)
+
+  // No need to verify for user validity since the middleware should prevent this request from running if the token is invalid
+  const user_id = user._id
+
+  const response = await chats.updateOne({user_id: user_id, session_id: session_id}, {
+    $set: {
+      chats: chats
+    }
+  })
+  const result = await response.json()
+  return result
+}
+
+// TBD: Define initial ticket generation here
+export const seed_tickets_collection = async() => {
+
+  
+}
