ce242b288cf73e5dda97fdb319945db079eca626
Joel Ethan Batac <bjoelethan@gmail.com>
2024-07-25 04:01:40 +0800

    feat: implement list virtualization
    Remove unused component board

--- src/app/components/Column.tsx
+++ src/app/components/Column.tsx
@@ -1,11 +1,10 @@
 'use client'
 
-import React, { useEffect, useState } from 'react'
+import React, { useCallback, useRef, useState } from 'react'
 import Ticket from './Ticket'
 import { motion } from 'framer-motion'
 import { ScrollArea } from "@/components/ui/scroll-area"
 import { ITicket } from '../../types'
-import { changeStatus } from '@/db/tickets'
 interface ColumnProps {
     title: string,
     status: string,
@@ -18,10 +17,30 @@ interface ColumnProps {
 
 export default function Column({title, status, tickets}: ColumnProps) {
 
+  const [numberOfTickets, setNumber] = useState(4)
+  const observer = useRef<IntersectionObserver>()
+  
+  const trigger = useCallback((node : HTMLElement | null) => {
+    if(observer.current) observer.current.disconnect()
+
+    observer.current = new IntersectionObserver(entries => {
+        if(numberOfTickets < tickets.length && entries[0].isIntersecting){
+            setNumber(prev => prev+4)
+        }
+    })
+    if(node) observer.current.observe(node)
+  }, [numberOfTickets])
+
+
+
+
+
+
+
     
   return (
     <motion.div layout
-
+        id={`#column-${title}`}
        className={`z-0 relative shadow-sm flex flex-col h-[calc(100vh-12rem)] bg-muted w-96 rounded-md p-3`}>
       
         {/* column header*/}
@@ -35,10 +54,13 @@ export default function Column({title, status, tickets}: ColumnProps) {
         <div className="h-full flex-auto gap-2 flex flex-col" data-test={`header-${title}`}> 
             {   // The handle drag and drop functions are inside ticket now
                 // Btw, consider moving these functions to a context if this gets refactored 
-                tickets.map((ticket, index) => {
-                    return <Ticket ticket={ticket} key={ticket.id}/> 
+                tickets.slice(0, numberOfTickets).map((ticket, index) => {
+                 
+                   return <Ticket ticket={ticket} key={ticket.id}/>
+                           
                 })
             }
+            <div ref={trigger}/>
         </div>
         </ScrollArea>
 
