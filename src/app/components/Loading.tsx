8ac2d403c81122ab82fe20fc7919ca4fcb7f16e3
khryssha <127598730+khryssha@users.noreply.github.com>
2024-07-30 16:42:01 +0800

    Add loading animation to nicebot, edit Chat.tsx to return only one conversation if specified

---
+++ src/app/components/Loading.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./styles.css";
+
+const Loading = () => {
+  return (
+    <>
+      <div className="align-balls bouncing-loader">
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </>
+  );
+};
+
+export default Loading;
